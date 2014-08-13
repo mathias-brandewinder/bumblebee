2db743f3ca15ee1a9d795354777c5442215c6b23
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2014-08-13 10:09:17 -0700

    Initial structure
    2 worker roles, fake config.

---
+++ CloudSwarm/QueenRole/WorkerRole.fs
@@ -0,0 +1,28 @@
+﻿namespace QueenRole
+
+open System
+open System.Collections.Generic
+open System.Diagnostics
+open System.Linq
+open System.Net
+open System.Threading
+open Microsoft.WindowsAzure
+open Microsoft.WindowsAzure.Diagnostics
+open Microsoft.WindowsAzure.ServiceRuntime
+
+type WorkerRole() =
+    inherit RoleEntryPoint() 
+
+    let log message (kind : string) = Trace.TraceInformation(message, kind)
+
+    override wr.Run() =
+
+        log "QueenRole entry point called" "Information"
+        while(true) do 
+            Thread.Sleep(10000)
+            log "Working" "Information"
+
+    override wr.OnStart() = 
+
+        ServicePointManager.DefaultConnectionLimit <- 12
+        base.OnStart()
